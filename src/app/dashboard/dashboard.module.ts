3151cdc9a1ff72856cee6a792f967219cce0317c
Earl Hans Genoso <earlhansg@github.com>
2020-10-01 23:49:48 +0800

    #2 Dashboard UI

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,6 +5,8 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 
 import { DashboardComponent } from './container/dashboard.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { HeaderBarComponent } from './components/header-bar/header-bar.component
     ],
     declarations: [
       DashboardComponent,
-      HeaderBarComponent
+      HeaderBarComponent,
+      SideBarComponent,
+      ContentComponent
     ]
 })
 export class DashboardModule {}
