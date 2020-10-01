ce26ff49f41c13271251c20699a2f52110dd6ef0
Earl Hans Genoso <earlhansg@github.com>
2020-10-02 01:33:06 +0800

    #5 Setup interface, service and enums

---
+++ src/app/shared/models/movie/movie.model.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  id: number;
+  name: string;
+  imageUrl: string;
+  ratings: number;
+  genre: string;
+  status?: string;
+  directed: string;
+  available: boolean;
+}
